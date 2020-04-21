6c79b9476b55e23fa6fb1f9f7cbdfa9c679bf461
Rakib Hossain <rakibhoossain@gmail.com>
2020-04-22 02:58:44 +0600

    area dynamic

--- admin/setting.php
+++ admin/setting.php
@@ -1,46 +1,66 @@
 <?php
 function wp_violet_register_settings() {
-   	add_option( 'wp_violet_narshindi_active', '0');
-   	register_setting( 'wp_violet_options_group', 'wp_violet_narshindi_active', 'wp_violet_callback' );
+	$items = ['palash', 'narshindi', 'sadar', 'raypur', 'shibpur', 'monohordi', 'belab', 'bangladesh', 'world'];
+	foreach ($items as $item) {
+		$key_active = 'wp_violet_'.$item.'_active';
+		$key_recovered = 'wp_violet_'.$item.'_recovered';
+		$key_fatal = 'wp_violet_'.$item.'_fatal';
 
-    add_option( 'wp_violet_narshindi_recovered', '0');
-   	register_setting( 'wp_violet_options_group', 'wp_violet_narshindi_recovered', 'wp_violet_callback' );
-
-    add_option( 'wp_violet_narshindi_fatal', '0');
-   	register_setting( 'wp_violet_options_group', 'wp_violet_narshindi_fatal', 'wp_violet_callback' );
+		add_option( $key_active, '0');
+	   	register_setting( 'wp_violet_options_group', $key_active, 'wp_violet_callback' );
+	    add_option( $key_recovered, '0');
+	   	register_setting( 'wp_violet_options_group', $key_recovered, 'wp_violet_callback' );
+	    add_option( $key_fatal, '0');
+	   	register_setting( 'wp_violet_options_group', $key_fatal, 'wp_violet_callback' );
+	}
 }
 add_action( 'admin_init', 'wp_violet_register_settings' );
 
 function wp_violet_register_options_page() {
-  add_options_page('Narshandi Corona', 'Narshandi Corona', 'manage_options', 'wp_violet', 'wp_violet_options_page');
+  add_options_page('Corona Update', 'Corona Update', 'manage_options', 'wp_violet', 'wp_violet_options_page');
 }
 add_action('admin_menu', 'wp_violet_register_options_page');
 ?>
 
-<?php function wp_violet_options_page()
+<?php 
+function wp_violet_options_page()
 {
 ?>
   <div>
   <?php screen_icon(); ?>
-  <h2>Corona cases in Narshindi</h2>
   <form method="post" action="options.php">
 	  <?php settings_fields( 'wp_violet_options_group' ); ?>
+
+<?php
+$items = ['palash', 'narshindi', 'sadar', 'raypur', 'shibpur', 'monohordi', 'belab', 'bangladesh', 'world'];
+
+foreach($items as $item){
+	$key_active = 'wp_violet_'.$item.'_active';
+	$key_recovered = 'wp_violet_'.$item.'_recovered';
+	$key_fatal = 'wp_violet_'.$item.'_fatal';
+?>
+
+<h2>Corona cases in <?php echo ucfirst($item); ?></h2>
 	  <table>
 		  <tr valign="top">
-		  	<th scope="row"><label for="wp_violet_narshindi_active">Active cases</label></th>
-		  	<td><input type="text" id="wp_violet_narshindi_active" name="wp_violet_narshindi_active" value="<?php echo get_option('wp_violet_narshindi_active'); ?>" /></td>
+		  	<th scope="row"><label for="<?php echo $key_active; ?>">Active cases</label></th>
+		  	<td><input type="text" id="<?php echo $key_active; ?>" name="<?php echo $key_active; ?>" value="<?php echo get_option($key_active); ?>" /></td>
 		  </tr>
 		  <tr valign="top">
-		  	<th scope="row"><label for="wp_violet_narshindi_recovered">Recovered cases</label></th>
-		  	<td><input type="text" id="wp_violet_narshindi_recovered" name="wp_violet_narshindi_recovered" value="<?php echo get_option('wp_violet_narshindi_recovered'); ?>" /></td>
+		  	<th scope="row"><label for="<?php echo $key_recovered; ?>">Recovered cases</label></th>
+		  	<td><input type="text" id="<?php echo $key_recovered; ?>" name="<?php echo $key_recovered; ?>" value="<?php echo get_option($key_recovered); ?>" /></td>
 		  </tr>
 		  <tr valign="top">
-		  	<th scope="row"><label for="wp_violet_narshindi_fatal">Fatal cases</label></th>
-		  	<td><input type="text" id="wp_violet_narshindi_fatal" name="wp_violet_narshindi_fatal" value="<?php echo get_option('wp_violet_narshindi_fatal'); ?>" /></td>
+		  	<th scope="row"><label for="<?php echo $key_fatal; ?>">Fatal cases</label></th>
+		  	<td><input type="text" id="<?php echo $key_fatal; ?>" name="<?php echo $key_fatal; ?>" value="<?php echo get_option($key_fatal); ?>" /></td>
 		  </tr>
 	  </table>
+
+<?php
+}
+?>
 	  <?php  submit_button(); ?>
   </form>
   </div>
 <?php
-} ?>
+}
